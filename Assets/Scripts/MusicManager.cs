a7ab41acd5d7b31c5a087c9857c9995466b53a5b
4happy-studio <4happy.studio@gmail.com>
2020-04-30 10:52:32 +0700

    Add Setting and credit menu

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public AudioClip[] audioClips;
+    AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        StartCoroutine(PlayMusic());
+    }
+
+    int GetRandomIndex()
+    {
+        return Random.Range(0, audioClips.Length);
+    }
+
+    IEnumerator PlayMusic()
+    {
+        while (true)
+        {
+            int index = GetRandomIndex();
+            audioSource.clip = audioClips[index];
+            audioSource.Play();
+            yield return new WaitForSeconds(audioClips[index].length);
+        }
+        
+
+    }
+}
---
+++ Assets/Scripts/SettingsManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SettingsManager : MonoBehaviour
+{
+    public TMP_Dropdown dropdownQuality;
+    public Slider sliderVolume;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+    }
+
+    public void SetQuality(int index)
+    {
+        QualitySettings.SetQualityLevel(index);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
