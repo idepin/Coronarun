15bbcfb5ca906c72a8bf186f461ecd6e4a246e6f
4happy-studio <4happy.studio@gmail.com>
2020-05-01 00:01:48 +0700

    Add script Tween

---
+++ Assets/Scripts/Animation/PingPongObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PingPongObject : MonoBehaviour
+{
+    public float maxY = 1f;
+    public float speed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        LeanTween.moveY(gameObject, maxY, speed).setEaseInExpo().setLoopPingPong();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
