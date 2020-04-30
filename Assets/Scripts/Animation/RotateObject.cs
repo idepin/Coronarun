070df053c69d8c9da43701dc9775e4f2426d3057
4happy-studio <4happy.studio@gmail.com>
2020-04-30 21:34:12 +0700

    add bigfan

---
+++ Assets/Scripts/Animation/RotateObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+    public float xSpeed = 5f, ySpeed = 0f, zSpeed = 0f;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(xSpeed, ySpeed, zSpeed);
+    }
+}
